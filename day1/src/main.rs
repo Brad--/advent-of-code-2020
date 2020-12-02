555b1c2363778d528d66b8ed6f875945145bf38d
Brad-- <brad.smith.1324@gmail.com>
2020-12-01 20:35:27 -0700

    k and n combo instead of hardcoded params

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,12 +1,17 @@
 use std::io::prelude::*;
+use std::io;
 use std::fs::File;
 use std::io::BufReader;
 
-fn main() -> std::io::Result<()> {
+use itertools::Itertools;
+
+fn read_file_to_vector() -> Result<Vec<i32>, io::Error> {
     let report = File::open("report.txt")?;
 
     let mut reader = BufReader::new(report);
     let mut line = String::new();
+
+    let mut expenses = Vec::new();
 
     loop {
         match reader.read_line(&mut line) {
@@ -14,7 +19,8 @@ fn main() -> std::io::Result<()> {
                 if bytes_read == 0 {
                     break;
                 }
-                print!("read: {}", line);
+                let line_as_int: i32 = line.trim().parse::<i32>().unwrap();
+                expenses.push(line_as_int);
                 line.clear();
             },
             Err(err) => {
@@ -22,6 +28,27 @@ fn main() -> std::io::Result<()> {
             }
         };
     }
-    
+
+    Ok(expenses)
+}
+
+fn k_combination_n_sum(expenses: &Vec<i32>, k: usize, n: i32) -> Option<Vec<i32>> {
+    expenses
+        .iter()
+        .copied()
+        .combinations(k)
+        .find(|combination| combination.iter().sum::<i32>() == n)
+}
+
+// Find entries in "report.txt" that sum to 2020, and print their product
+fn main() -> std::io::Result<()> {
+
+    let expenses = read_file_to_vector()?;
+
+    let combinations = k_combination_n_sum(&expenses, 3, 2020);
+
+    let product: i32 = combinations.unwrap().iter().product();
+
+    println!("Product of combination: {}", product);
     Ok(())
 }
